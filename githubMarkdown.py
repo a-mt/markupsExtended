cc7b9165b2ae39d6ab3a4e3d96fee008af36a1ce
a-mt <amistri@live.fr>
2018-12-10 15:21:37 +0100

    Add pre blocks highlighting

--- githubMarkdown.py
+++ githubMarkdown.py
@@ -4,6 +4,11 @@ import mistletoe
 import markups.common as common
 from markups.abstract import AbstractMarkup, ConvertedMarkup
 
+from pygments import highlight
+from pygments.styles import get_style_by_name as get_style
+from pygments.lexers import get_lexer_by_name as get_lexer, guess_lexer
+from pygments.formatters.html import HtmlFormatter
+
 class GithubMarkdownMarkup(AbstractMarkup):
 
     name                 = 'GithubMarkdown'
@@ -14,14 +19,24 @@ class GithubMarkdownMarkup(AbstractMarkup):
     def __init__(self, filename):
         self.filename  = filename
 
+    # Convert a Markdown text to HTML using mitletoe (GFM parser)
     def convert(self, text):
         title = ''
-        body  = mistletoe.markdown(text) + '\n'
+        body  = mistletoe.markdown(text, renderer=PygmentsRenderer) + '\n'
 
-        # Determine stylesheet
-        if any(extension.endswith('codehilite') for extension in self.requested_extensions):
-            stylesheet = common.get_pygments_stylesheet('.codehilite')
-        else:
-            stylesheet = ''
+        return ConvertedMarkup(body, title)
 
-        return ConvertedMarkup(body, title, stylesheet)
+# Highlight pre blocks using Pygments
+class PygmentsRenderer(mistletoe.HTMLRenderer):
+    formatter = HtmlFormatter()
+    formatter.noclasses = True
+
+    def __init__(self, *extras, style='default'):
+        super().__init__(*extras)
+        self.formatter.style = get_style(style)
+
+
+    def render_block_code(self, token):
+        code = token.children[0].content
+        lexer = get_lexer(token.language) if token.language else guess_lexer(code)
+        return highlight(code, lexer, self.formatter)
